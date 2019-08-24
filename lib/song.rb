3f6bc3e11c7b48859034830f87fe8e940ed8d7b8
Sam Markham <sam.e.markham@gmail.com>
2019-08-24 13:53:21 -0700

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -8,9 +8,11 @@ class Song
     self.to_s.downcase.pluralize
   end
 
+  #Return value of this method is: ["id", "name", "album"] if calling on a songs table.
   def self.column_names
     DB[:conn].results_as_hash = true
 
+    #this bit grabs a huge amount of key-value info. The key "name" gives us column name, and "type" gives type
     sql = "pragma table_info('#{table_name}')"
 
     table_info = DB[:conn].execute(sql)
@@ -21,11 +23,15 @@ class Song
     column_names.compact
   end
 
+  #the conversion to symbols here is bc attr_accessors must be symbols
   self.column_names.each do |col_name|
     attr_accessor col_name.to_sym
   end
 
+  #options defaults to empty hash but usually we'll be passing a hash in
   def initialize(options={})
+    #using the send keyword to set the key in this hash to the variable and then setting that equal to the value
+    #like name = the name value passed in. This works as long as there are attr_accessors for everything.
     options.each do |property, value|
       self.send("#{property}=", value)
     end
@@ -41,14 +47,21 @@ class Song
     self.class.table_name
   end
 
+  #this is grabbing the values we want to insert into table for making new record
   def values_for_insert
     values = []
     self.class.column_names.each do |col_name|
+      #the unless nil is to deal with id, which isn't set at this point bc this is for INSERT
+      #invoking attr_reader methods without having to name them by using send
+      #return value wrapped in single quotes within string bc they need to be strings within SQL string
       values << "'#{send(col_name)}'" unless send(col_name).nil?
     end
+    #values array made into string bc SQL queries need to be string
     values.join(", ")
   end
 
+  #we are deleting id from here bc the table generates its own id
+  #this also turns the array into a comma-separated string so it's formatted like argument
   def col_names_for_insert
     self.class.column_names.delete_if {|col| col == "id"}.join(", ")
   end
